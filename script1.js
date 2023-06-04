e64d1cd6772137685382d510bb79ee8b45b352d8
eloaandreatta <111518085+eloaandreatta@users.noreply.github.com>
2023-06-04 11:13:40 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,20 @@
+function sim() {
+    alert("também te amo meu bebê, agora vem me comer ao som de:");
+    // redireciona para um URL após clicar no SIM
+    location.href = "https://youtu.be/izGwDsrQ1eQ";
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
+
+/* Apague se quiser, isso apenas escreve MTHS1901 e o link para o tutorial no console */
+const o = "MTHS1901", e = 90, l = "bold"; console.log(`%c${o}`, "font-size: 90px; font-weight: bold; background: linear-gradient(90deg, red, yellow);"), console.log("Tutorial: https://www.youtube.com/watch?v=zxxB9SFh9p4");
